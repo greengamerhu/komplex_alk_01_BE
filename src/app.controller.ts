61357b540678c121fed32d27c1562bcafc4e6f4f
Rimóczi Dániel <rimoczidani007@gmail.com>
2023-01-24 14:56:59 +0100

    Done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,32 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
+  Param,
+  Post,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+import { tarhelyDto } from './tarhelyDto.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Get('/api/tarhely')
+  async getTarhely() {
+    const [rows] = await db.execute("SELECT * FROM tarhelycsomagok ")
+    return {rows : rows};
+  }
+  @Post('/api/tarhely')
+  async postTarhely(@Body() tarhelyData : tarhelyDto) {
+      await db.execute("INSERT INTO tarhelycsomagok (nev, meret, ar) VALUES(?,?,?)", [tarhelyData.nev, tarhelyData.meret, tarhelyData.ar])
+  };
+
+  @Delete('/api/tarhely/:id')
+  async deleteUserApi(@Param('id') id: number) {
+    await db.execute('DELETE FROM `tarhelycsomagok` WHERE `id` = ?', [id])
   }
 }
---
+++ src/tarhelyDto.dto.ts
@@ -0,0 +1,5 @@
+export class tarhelyDto {
+    nev : string;
+    meret : number;
+    ar : number;
+}
